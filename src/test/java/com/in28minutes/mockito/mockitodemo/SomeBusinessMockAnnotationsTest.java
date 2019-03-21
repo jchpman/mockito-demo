85586a2e4c72fd4d28accf074901ee63ad20dec6
jchpmn <josephchapman@hotmail.com>
2019-03-20 20:15:55 -0500

    update 3-20-19

---
+++ src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessMockAnnotationsTest.java
@@ -0,0 +1,46 @@
+package com.in28minutes.mockito.mockitodemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SomeBusinessMockAnnotationsTest {
+
+    @Mock
+    DataService dataServiceMock;
+
+    @InjectMocks
+    SomeBusinessImpl businessImpl;
+
+    @Test
+    public void testFindTheGreatestFromAllData(){
+
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {24,15,3});
+        assertEquals(24, businessImpl.findTheGreatestFromAllData());
+    }
+
+    @Test
+    public void testFindTheGreatestFromAllData_ForOneValue(){
+
+        DataService dataServiceMock = mock(DataService.class);
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {15});
+        SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
+        int result = businessImpl.findTheGreatestFromAllData();
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void testFindTheGreatestFromAllData_NoValues(){
+
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {});
+        assertEquals(Integer.MIN_VALUE, businessImpl.findTheGreatestFromAllData());
+    }
+}
+
